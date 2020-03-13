37ed4dc60b5960a746858efacb1a060a002d9153
Tom Lee <tomtleee@gmail.com>
2020-03-13 09:17:49 -0400

    Add Carto data as layer to map

--- js/main.js
+++ js/main.js
@@ -1,18 +1,25 @@
 require([
     "esri/Map",
+    "esri/layers/GeoJSONLayer",
     "esri/views/MapView"
 ], function(
     Map,
+    GeoJSONLayer,
     MapView
 ) {
+    const pointsLayer = GeoJSONLayer({
+        url: "https://tomtl.carto.com/api/v2/sql?format=GeoJSON&q=SELECT fid as id, the_geom from wmnf_activity_points limit 9"
+    });
+
     const map = new Map({
-        basemap: "topo-vector"
+        basemap: "topo-vector",
+        layers: [pointsLayer]
     });
 
     const view = new MapView({
         container: "viewDiv",
         map: map,
-        zoom: 4,
-        center: [15, 65]
+        zoom: 9,
+        center: [-71.5, 44.1]
     });
 });
