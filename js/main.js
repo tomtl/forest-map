1aba17d95745aab22b84bf818e66ec8599ba31eb
Tom Lee <tomtleee@gmail.com>
2020-03-16 09:36:24 -0400

    Set up layers

--- js/main.js
+++ js/main.js
@@ -7,13 +7,40 @@ require([
     GeoJSONLayer,
     MapView
 ) {
-    const pointsLayer = GeoJSONLayer({
-        url: "https://tomtl.carto.com/api/v2/sql?format=GeoJSON&q=SELECT fid as id, the_geom from wmnf_activity_points limit 9"
-    });
+    class ParkLayer {
+        constructor(name, sql) {
+            this.name = name;
+            this.sql = sql;
+            this.url = ("https://tomtl.carto.com/api/v2/sql?format=GeoJSON&q=" + sql);
+            this.layer = GeoJSONLayer({url: this.url});
+        }
+    };
+
+    class ActivityLayer extends ParkLayer {
+        constructor(name, sql) {
+            super(name, sql);
+            this.url = ("https://tomtl.carto.com/api/v2/sql?format=GeoJSON&q=SELECT fid, the_geom from wmnf_activity_points WHERE marker_activity_group = '" + sql + "'");
+            this.layer = GeoJSONLayer({url: this.url});
+        }
+    };
+
+    const campingLayer = new ActivityLayer('camping', 'Camping and Cabins');
+    const hikingLayer = new ActivityLayer('hiking', 'Hiking');
+    const overlookLayer = new ActivityLayer('overlook', 'Nature Viewing');
+    const picnicLayer = new ActivityLayer('picnic', 'Picnicking');
+    const trailLayer = new ParkLayer('trails', 'SELECT fid, the_geom from wmnf_trail_lines');
+    const reviewLayer = new ParkLayer('reviews',  'SELECT fid, the_geom from wmnf_user_reviews');
 
     const map = new Map({
         basemap: "topo-vector",
-        layers: [pointsLayer]
+        layers: [
+            campingLayer.layer,
+            hikingLayer.layer,
+            overlookLayer.layer,
+            picnicLayer.layer,
+            trailLayer.layer,
+            reviewLayer.layer
+        ]
     });
 
     const view = new MapView({
