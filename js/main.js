5ed3a7099e163425863494a81f986f7007fb25b9
Tom Lee <tomtleee@gmail.com>
2020-03-17 09:15:54 -0400

    Symbolize layers

--- js/main.js
+++ js/main.js
@@ -8,37 +8,98 @@ require([
     MapView
 ) {
     class ParkLayer {
-        constructor(name, sql) {
+        constructor(name, sql, renderer) {
             this.name = name;
             this.sql = sql;
             this.url = ("https://tomtl.carto.com/api/v2/sql?format=GeoJSON&q=" + sql);
-            this.layer = GeoJSONLayer({url: this.url});
+            this.layer = GeoJSONLayer({
+                url: this.url,
+                renderer: renderer
+            });
         }
     };
 
     class ActivityLayer extends ParkLayer {
-        constructor(name, sql) {
+        constructor(name, sql, symbol) {
             super(name, sql);
+            this.symbol = symbol;
             this.url = ("https://tomtl.carto.com/api/v2/sql?format=GeoJSON&q=SELECT fid, the_geom from wmnf_activity_points WHERE marker_activity_group = '" + sql + "'");
-            this.layer = GeoJSONLayer({url: this.url});
+            this.layer = GeoJSONLayer({
+                title: this.name,
+                url: this.url,
+                renderer: {
+                    type: "simple",
+                    symbol: {
+                        type: "web-style",
+                        styleName: "Esri2DPointSymbolsStyle",
+                        name: this.symbol
+                    },
+                    visualVariables: [{
+                        type: "size",
+                        valueExpression: "$view.scale",
+                        stops: [
+                            { value: 36112, size: 20 },
+                            { value: 36112 * 2, size: 16 },
+                            { value: 36112 * 8, size: 12 },
+                            { value: 36112 * 32, size: 6 }
+                        ]
+                    }]
+                }
+            });
         }
     };
 
-    const campingLayer = new ActivityLayer('camping', 'Camping and Cabins');
-    const hikingLayer = new ActivityLayer('hiking', 'Hiking');
-    const overlookLayer = new ActivityLayer('overlook', 'Nature Viewing');
-    const picnicLayer = new ActivityLayer('picnic', 'Picnicking');
-    const trailLayer = new ParkLayer('trails', 'SELECT fid, the_geom from wmnf_trail_lines');
-    const reviewLayer = new ParkLayer('reviews',  'SELECT fid, the_geom from wmnf_user_reviews');
+    const trailRenderer = {
+        type: "simple",
+        symbol: {
+            type: "simple-line",
+            color: "#42855A"
+        },
+        visualVariables: [{
+            type: "size",
+            valueExpression: "$view.scale",
+            stops: [
+                { value: 36112, size: "2px" },
+                { value: 36112 * 2, size: "1px" },
+                { value: 36112 * 32, size: "0.5px" }
+            ]
+        }]
+    };
+
+    const reviewRenderer = {
+        type: "simple",
+        symbol: {
+            type: "web-style",
+            styleName: "Esri2DPointSymbolsStyle",
+            name: "star"
+        },
+        visualVariables: [{
+            type: "size",
+            valueExpression: "$view.scale",
+            stops: [
+                { value: 36112, size: 16 },
+                { value: 36112 * 2, size: 12 },
+                { value: 36112 * 8, size: 10 },
+                { value: 36112 * 32, size: 4 }
+            ]
+        }]
+    };
+
+    const campingLayer = new ActivityLayer('camping', 'Camping and Cabins', 'campground');
+    const hikingLayer = new ActivityLayer('hiking', 'Hiking', 'trail');
+    const overlookLayer = new ActivityLayer('overlook', 'Nature Viewing', 'landmark');
+    const picnicLayer = new ActivityLayer('picnic', 'Picnicking', 'restaurant');
+    const trailLayer = new ParkLayer('trails', 'SELECT fid, the_geom from wmnf_trail_lines', trailRenderer);
+    const reviewLayer = new ParkLayer('reviews',  'SELECT fid, the_geom from wmnf_user_reviews', reviewRenderer);
 
     const map = new Map({
         basemap: "topo-vector",
         layers: [
+            trailLayer.layer,
             campingLayer.layer,
             hikingLayer.layer,
             overlookLayer.layer,
             picnicLayer.layer,
-            trailLayer.layer,
             reviewLayer.layer
         ]
     });
