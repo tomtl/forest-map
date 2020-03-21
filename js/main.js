b0a3bb7c08351260f085c87dd63dfe1a3bc42017
Tom Lee <tomtleee@gmail.com>
2020-03-21 09:33:06 -0400

    User submit review

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+let x = 0;
+let y = 0;
+
 require([
     "esri/Map",
     "esri/layers/GeoJSONLayer",
@@ -120,20 +123,99 @@ require([
         // message window appears telling user to click the map
         document.getElementById('footer').innerHTML = '<p>Click map where you want to review.</p>';
 
+        // validate the form and disable the submit button if not valid
+        (function() {
+          'use strict';
+          window.addEventListener('load', function() {
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            var forms = document.getElementsByClassName('review-form');
+            // Loop over them and prevent submission
+            var validation = Array.prototype.filter.call(forms, function(form) {
+              form.addEventListener('submit', function(event) {
+                if (form.checkValidity() === false) {
+                  event.preventDefault();
+                  event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+              }, false);
+            });
+          }, false);
+        })();
+
         // user clicks the map and coords are returned
         view.on(['pointer-down'], function(evt){
             console.log(evt);
             let pt = view.toMap({x: evt.x, y: evt.y});
-            let y = pt.latitude.toFixed(7);
-            let x = pt.longitude.toFixed(7);
+            y = pt.latitude.toFixed(7);
+            x = pt.longitude.toFixed(7);
             console.log("Clicked location: ", x, y);
 
             // user enters review information
             $('#footer').hide();
+            $('#viewDiv').css("height", "50%");
             $('#add-review-text').show();
+
+            var form = document.getElementsByClassName('review-form');
+            // form.addEventListener('submit', function(event) {
+
         });
     });
 
-    // 5- user clicks submit and review gets added to DB
+    // 5-
     // 6- review gets added to map or map refreshes to show review
+
 });
+
+function submitReview(){
+    console.log("Someone clicked it!");
+    var username = document.getElementById("inputName").value;
+    var rating = document.getElementById("inputRating").value;
+    var review = document.getElementById("inputReview").value;
+    console.log("NAME: ", username);
+    console.log("RATING: ", rating);
+    console.log("REVIEW: ", review);
+    console.log("COORDS: ", x, y);
+    addReview(username, rating, review, y, x);
+};
+
+function addReview(name, rating, review, latitude, longitude) {
+    const m = new Date();
+    const dateString =
+        m.getUTCFullYear() + "-" +
+        ("0" + (m.getUTCMonth()+1)).slice(-2) + "-" +
+        ("0" + m.getUTCDate()).slice(-2) + "T" +
+        ("0" + m.getUTCHours()).slice(-2) + ":" +
+        ("0" + m.getUTCMinutes()).slice(-2) + ":" +
+        ("0" + m.getUTCSeconds()).slice(-2)
+    ;
+
+    const geom = "ST_SetSRID(ST_MakePoint(" + longitude + ", " + latitude + "), 4326)";
+    const validated = "TRUE";
+
+    const valuesString = "('" + name + "', '" + dateString  + "', " + rating + ", '" + review + "', " + validated + ", " + geom + ")";
+
+    const url = "https://tomtl.carto.com/api/v2/sql"
+    const sql = "INSERT INTO wmnf_user_reviews (username, date, rating, review, validated, the_geom) VALUES " + valuesString;
+
+    postUpdate(url, sql);
+};
+
+function postUpdate(url, sql) {
+    $.post( url, {
+        q: sql,
+        api_key: 'LHES7OUmRgnT6zZVkJFV1w'
+        })
+        .done(function(data){
+            console.log("data loaded");
+        })
+        .fail(function(data){
+            console.error("load failed");
+            console.log("URL: " + url);
+            console.log("SQL: " + sql);
+            console.log(data.responseText);
+            console.log(data);
+        })
+        .always(function(data){
+            console.log("Finished load");
+        })
+};
